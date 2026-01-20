c6f93c3ab1fc029243024c8555b1125543a2b3b7
Evelyn <velyn18morocho@gmail.com>
2026-01-20 18:17:12 -0500

    Se agrega modelo tarea y se genera las migraciones

--- pagina_web/lista_tareas/models.py
+++ pagina_web/lista_tareas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    ESTADOS=(
+        ('1','Pendiente'),
+        ('2','En_progreso'),
+        ('3','Completado'),
+    )
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, blank=True)
+    estado = models.IntegerField(default=0)
+    fecha_creacion = models.DateField(auto_now_add=True)
